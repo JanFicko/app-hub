ac4634d23c688975bf0031378fd6734eae756b95
Jan Ficko <jan.ficko@margento.com>
2019-03-22 14:20:47 +0100

    Changed gitignore in BackEnd project

--- BackEnd/config.js
+++ BackEnd/config.js
@@ -5,7 +5,7 @@ let MONGODB_PORT = "27017";
 let MONGODB_DATABASE_NAME = "app-hub";
 
 let GITLAB_IP = "http://localhost/api/v4/";
-let GITLAB_PRIVATE_TOKEN = "YOUR_PRIVATE_GITLAB_TOKEN";
+let GITLAB_PRIVATE_TOKEN = "PRIVATE_GITLAB_TOKEN";
 
 let ANDROID_GROUP = "groups/8";
 let IOS_GROUP = "groups/9";
