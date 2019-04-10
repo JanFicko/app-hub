33624e89e628d3534c72c70a4a448ff0e28288d0
Jan <jan.ficko@gmail.com>
2019-04-10 15:18:16 +0200

    Added additional attributes to config.js

--- BackEnd/config.js
+++ BackEnd/config.js
@@ -1,22 +1,35 @@
+let DOMAIN_NAME = "localhost:3000";
+
 let WEB_PORT = "3000";
+
+let JWT_SECRET = "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING";
 
 let MONGODB_IP = "localhost";
 let MONGODB_PORT = "27017";
 let MONGODB_DATABASE_NAME = "app-hub";
 
 let GITLAB_IP = "http://localhost/api/v4/";
+let GITLAB_IP_SUFFIX = "api/v4/";
 let GITLAB_PRIVATE_TOKEN = "PRIVATE_GITLAB_TOKEN";
 
 let ANDROID_GROUP = "groups/8";
 let IOS_GROUP = "groups/9";
 
+let VERSION_REGEX = "^v[0-9]+.[0-9]+.[0-9]+$";
+let NO_VERSION_TEXT = "No version";
+
 module.exports = {
+    DOMAIN_NAME,
+    JWT_SECRET,
     WEB_PORT,
     MONGODB_IP,
     MONGODB_PORT,
     MONGODB_DATABASE_NAME,
     GITLAB_IP,
+    GITLAB_IP_SUFFIX,
     GITLAB_PRIVATE_TOKEN,
     ANDROID_GROUP,
-    IOS_GROUP
+    IOS_GROUP,
+    VERSION_REGEX,
+    NO_VERSION_TEXT
 };
