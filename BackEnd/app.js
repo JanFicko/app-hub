a60fbdd58b5ac788be7b047375935143e0f9beb5
Jan Ficko <jan.ficko@gmail.com>
2019-04-19 15:11:43 +0200

    - Added CORS

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -32,6 +32,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Headers", '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    next();
+});
 app.use('/api/projects', require('./routes/projects'));
 app.use('/api/users', require('./routes/users'));
 app.use(express.static(__dirname + '/public'));
